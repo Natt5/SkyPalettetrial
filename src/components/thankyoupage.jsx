07cdb81935b891cfb1bc8bb8fb7a0665e17f6423
Natt5 <152211400+Natt5@users.noreply.github.com>
2024-03-21 16:27:34 +0300

    Created thankyoupage.jsx

---
+++ src/components/thankyoupage.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function ThankYou() {
+  return (
+    <div>
+      <h2>Thank You!</h2>
+      <p>Your submission has been received. We will be in touch soon.</p>
+    </div>
+  );
+}
+
+export default ThankYou;
