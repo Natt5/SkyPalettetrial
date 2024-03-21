bf444e219945170d8ee1fa51111405d3a9bd4c28
Natt5 <152211400+Natt5@users.noreply.github.com>
2024-03-21 17:17:52 +0300

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -140,7 +140,8 @@ function Contact() {
                 </div>
             )}
             {/* Add 'method="POST"', 'data-netlify="true"', and a hidden input for form name */}
-           <form name="contact" method="POST" data-netlify="true" action="/thankyoupage">
+        <form name="contact" method="POST" data-netlify="true" action="/thankyoupage">
+
                 {/* Netlify requires a hidden input field to identify the form */}
                 <input type="hidden" name="form-name" value="contact" />
                 <div className="mb-3">
